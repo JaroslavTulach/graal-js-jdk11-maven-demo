94fcd6b5b50dafc5a923b892f6ad38a77a20dc7e
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2018-12-13 11:26:57 +0100

    Use classes rather than functions to show another Graal.js benefit over Nashorn

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -21,19 +21,20 @@ public class App {
             + "var N = 2000;\n"
             + "var EXPECTED = 17393;\n"
             + "\n"
-            + "function Natural() {\n"
-            + "    x = 2;\n"
-            + "    return {\n"
-            + "        'next' : function() { return x++; }\n"
-            + "    };\n"
+            + "class Natural {\n"
+            + "  constructor() {\n"
+            + "    this.x = 2;\n"
+            + "  }\n"
+            + "  next() { return this.x++; }\n"
             + "}\n"
             + "\n"
-            + "function Filter(number, filter) {\n"
-            + "    var self = this;\n"
+            + "class Filter {\n"
+            + "  constructor (number, filter) {\n"
             + "    this.number = number;\n"
             + "    this.filter = filter;\n"
-            + "    this.accept = function(n) {\n"
-            + "      var filter = self;\n"
+            + "  }\n"
+            + "  accept(n) {\n"
+            + "      var filter = this;\n"
             + "      for (;;) {\n"
             + "          if (n % filter.number === 0) {\n"
             + "              return false;\n"
@@ -44,28 +45,27 @@ public class App {
             + "          }\n"
             + "      }\n"
             + "      return true;\n"
-            + "    };\n"
-            + "    return this;\n"
+            + "  }\n"
             + "}\n"
             + "\n"
-            + "function Primes(natural) {\n"
-            + "    var self = this;\n"
+            + "class Primes {\n"
+            + "  constructor (natural) {\n"
             + "    this.natural = natural;\n"
             + "    this.filter = null;\n"
-            + "\n"
-            + "    this.next = function() {\n"
+            + "  }\n"
+            + "  next() {\n"
             + "        for (;;) {\n"
-            + "            var n = self.natural.next();\n"
-            + "            if (self.filter === null || self.filter.accept(n)) {\n"
-            + "                self.filter = new Filter(n, self.filter);\n"
+            + "            var n = this.natural.next();\n"
+            + "            if (this.filter === null || this.filter.accept(n)) {\n"
+            + "                this.filter = new Filter(n, this.filter);\n"
             + "                return n;\n"
             + "            }\n"
             + "        }\n"
-            + "    };\n"
+            + "  }\n"
             + "}\n"
             + "\n"
             + "function primesMain() {\n"
-            + "    var primes = new Primes(Natural());\n"
+            + "    var primes = new Primes(new Natural());\n"
             + "    var primArray = [];\n"
             + "    for (var i=0;i<=N;i++) { primArray.push(primes.next()); }\n"
             + "    if (primArray[N] != EXPECTED) { throw new Error('wrong prime found: '+primArray[N]); }\n"
@@ -103,8 +103,12 @@ public class App {
         ScriptEngine nashornEngine = new ScriptEngineManager().getEngineByName("nashorn");
         if (nashornEngine == null) {
             System.out.println("*** Nashorn not found ***");
-            return 0;
+            return Long.MAX_VALUE;
         } else {
+            if (SOURCE.contains("class ")) {
+                System.out.println("*** Nashorn doesn't support ES6");
+                return Long.MAX_VALUE;
+            }
             return benchScriptEngineIntl(nashornEngine);
         }
     }
@@ -137,7 +141,7 @@ public class App {
                 System.out.println("iteration: " + (took));
             }
         } catch (Exception ex) {
-            System.out.println(ex);
+            throw new IOException(ex);
         }
         return took;
     }
