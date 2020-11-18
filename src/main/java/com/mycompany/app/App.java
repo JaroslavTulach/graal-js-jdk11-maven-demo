f4f588f75d931206a53bcbd2e3ec19224f494a74
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2020-11-18 10:37:20 +0100

    Put JavaScript into its own App.js file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -46,7 +46,9 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptEngine;
 import javax.script.Invocable;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import org.graalvm.polyglot.Source;
+import java.net.URL;
 
 /**
  * Simple benchmark for Graal.js via GraalVM Polyglot Context and ScriptEngine.
@@ -56,60 +58,6 @@ public class App {
     public static final int WARMUP = 15;
     public static final int ITERATIONS = 10;
     public static final String BENCHFILE = "src/bench.js";
-
-    public static final String SOURCE = ""
-            + "var N = 2000;\n"
-            + "var EXPECTED = 17393;\n"
-            + "\n"
-            + "function Natural() {\n"
-            + "    x = 2;\n"
-            + "    return {\n"
-            + "        'next' : function() { return x++; }\n"
-            + "    };\n"
-            + "}\n"
-            + "\n"
-            + "function Filter(number, filter) {\n"
-            + "    var self = this;\n"
-            + "    this.number = number;\n"
-            + "    this.filter = filter;\n"
-            + "    this.accept = function(n) {\n"
-            + "      var filter = self;\n"
-            + "      for (;;) {\n"
-            + "          if (n % filter.number === 0) {\n"
-            + "              return false;\n"
-            + "          }\n"
-            + "          filter = filter.filter;\n"
-            + "          if (filter === null) {\n"
-            + "              break;\n"
-            + "          }\n"
-            + "      }\n"
-            + "      return true;\n"
-            + "    };\n"
-            + "    return this;\n"
-            + "}\n"
-            + "\n"
-            + "function Primes(natural) {\n"
-            + "    var self = this;\n"
-            + "    this.natural = natural;\n"
-            + "    this.filter = null;\n"
-            + "\n"
-            + "    this.next = function() {\n"
-            + "        for (;;) {\n"
-            + "            var n = self.natural.next();\n"
-            + "            if (self.filter === null || self.filter.accept(n)) {\n"
-            + "                self.filter = new Filter(n, self.filter);\n"
-            + "                return n;\n"
-            + "            }\n"
-            + "        }\n"
-            + "    };\n"
-            + "}\n"
-            + "\n"
-            + "function primesMain() {\n"
-            + "    var primes = new Primes(Natural());\n"
-            + "    var primArray = [];\n"
-            + "    for (var i=0;i<=N;i++) { primArray.push(primes.next()); }\n"
-            + "    if (primArray[N] != EXPECTED) { throw new Error('wrong prime found: '+primArray[N]); }\n"
-            + "}\n";
 
     public static void main(String[] args) throws Exception {
         benchGraalPolyglotContext();
@@ -121,7 +69,9 @@ public class App {
         System.out.println("=== Graal.js via org.graalvm.polyglot.Context === ");
         long took = 0;
         try (Context context = Context.create()) {
-            context.eval(Source.newBuilder("js", SOURCE, "src.js").build());
+            URL src = App.class.getResource("App.js");
+            assert src != null;
+            context.eval(Source.newBuilder("js", src).build());
             Value primesMain = context.getBindings("js").getMember("primesMain");
             System.out.println("warming up ...");
             for (int i = 0; i < WARMUP; i++) {
@@ -163,7 +113,9 @@ public class App {
     private static long benchScriptEngineIntl(ScriptEngine eng) throws IOException {
         long took = 0L;
         try {
-            eng.eval(SOURCE);
+            URL src = App.class.getResource("App.js");
+            assert src != null;
+            eng.eval(new InputStreamReader(src.openStream()));
             Invocable inv = (Invocable) eng;
             System.out.println("warming up ...");
             for (int i = 0; i < WARMUP; i++) {
